df66c0707aff56f4daa856b15f869025e31a5b1e
Yunier Alvarez <yunieralvarez@gmail.com>
2022-01-01 21:28:42 -0800

    rename

---
+++ src/prop-to-str.util.ts
@@ -0,0 +1,8 @@
+export const propToStr = <T>(expression: (model: T) => unknown): string => {
+  const expStr = expression.toString();
+  const expSplitOnReturn = expStr.split('return');
+  const expSplitOnDot = expSplitOnReturn.slice(-1)[0].split('.');
+  const propArr = expSplitOnDot.slice(-1)[0].split(';');
+  const prop = propArr[0];
+  return prop;
+};
