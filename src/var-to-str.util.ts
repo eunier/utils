54a615c70307288679967e501a97782d738a8123
Yunier Alvarez <yunieralvarez@gmail.com>
2022-01-02 12:04:42 -0800

    restore

--- src/var-to-str.util.ts
+++ src/var-to-str.util.ts
@@ -1,14 +1,14 @@
 import { WORD } from './constants';
 
-// export const varToStr = (expression: () => void): string => {
-//   const codeStr = expression.toString();
-//   const match = codeStr.match(WORD);
+export const varToStr = (expression: () => void): string => {
+  const codeStr = expression.toString();
+  const match = codeStr.match(WORD);
 
-//   if (!match) {
-//     throw Error(`Invalid expression '${codeStr}'.`);
-//   }
+  if (!match) {
+    throw Error(`Invalid expression '${codeStr}'.`);
+  }
 
-//   const word = match.slice(-1)[0];
+  const word = match.slice(-1)[0];
 
-//   return word;
-// };
+  return word;
+};
