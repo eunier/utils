2cabd35f5e85f3caa7c27fc2ec1ab8925fecd431
Yunier Alvarez <yunieralvarez@gmail.com>
2022-01-27 18:45:04 -0500

    rafactor

--- src/pick-random.util.test.ts
+++ src/pick-random.util.test.ts
@@ -2,12 +2,13 @@ import { range } from 'ramda';
 import { pickRandom } from './pick-random.util';
 
 describe(pickRandom.name, () => {
-  test('should always return random a value from the array', () => {
+  test('should always return random a value from an array', () => {
+    const twoDArr = [2, 9, 99]
+      .map(num => range(0, num))
+      .map(r => r.map(() => true));
+
     range(0, 99).forEach(() =>
-      [2, 9, 99]
-        .map(num => range(0, num))
-        .map(r => r.map(() => true))
-        .forEach(arr => expect(pickRandom(arr)).not.toBe(undefined))
+      twoDArr.forEach(arr => expect(pickRandom(arr)).not.toBe(undefined))
     );
   });
 });
