a3b4e7d072184b75f9484e3c693bdc8208a327a5
Yunier Alvarez <yunieralvarez@gmail.com>
2022-01-27 18:40:24 -0500

    refactor

--- src/pick-random.util.test.ts
+++ src/pick-random.util.test.ts
@@ -2,11 +2,12 @@ import { range } from 'ramda';
 import { pickRandom } from './pick-random.util';
 
 describe(pickRandom.name, () => {
-  test('should always return a value from the array', () => {
+  test('should always return random a value from the array', () => {
     range(0, 99).forEach(() =>
-      [range(0, 2), range(0, 9), range(0, 99)]
+      [2, 9, 99]
+        .map(num => range(0, num))
         .map(r => r.map(() => true))
-        .forEach(arr => expect(arr).not.toBe(undefined))
+        .forEach(arr => expect(pickRandom(arr)).not.toBe(undefined))
     );
   });
 });
