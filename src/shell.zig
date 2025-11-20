9438c18f86189a047d0ce8f288473b373f12fe92
Yunier Alvarez <yunieralvarez@gmail.com>
2025-11-20 07:32:57 -0500

    sync

--- src/shell.zig
+++ src/shell.zig
@@ -2,28 +2,11 @@ const std = @import("std");
 const fmt_mod = std.fmt;
 const mem = std.mem;
 const process = std.process;
+const posix = std.posix;
 
 const log = std.log.scoped(.dep_util_shell);
 
-pub fn runInteractive(alc: mem.Allocator, comptime fmt: []const u8, args: anytype) !process.Child.Term {
-    const cmd = try fmt_mod.allocPrint(alc, fmt, args);
-    defer alc.free(cmd);
-
-    var process_child = process.Child.init(
-        &[_][]const u8{ "/usr/bin/env", "bash", "-c", cmd },
-        alc,
-    );
-
-    process_child.stderr_behavior = .Inherit;
-    process_child.stdin_behavior = .Inherit;
-    process_child.stdout_behavior = .Inherit;
-    log.debug("{s}", .{cmd});
-    return try process_child.spawnAndWait();
-}
-
-/// Spawns a child process, waits for it, collecting stdout and stderr, and then returns. If it
-/// succeeds, the caller owns result.stdout and result.stderr memory.
-pub fn runCapture(
+pub fn exec(
     alc: mem.Allocator,
     comptime fmt: []const u8,
     args: anytype,
@@ -35,7 +18,28 @@ pub fn runCapture(
     return process.Child.run(
         .{
             .allocator = alc,
-            .argv = &[_][]const u8{ "/usr/bin/env", "bash", "-c", cmd },
+            .argv = &[_][]const u8{ shell(), "-c", cmd },
         },
     );
+}
+
+pub fn execInteractive(alc: mem.Allocator, comptime fmt: []const u8, args: anytype) !process.Child.Term {
+    const cmd = try fmt_mod.allocPrint(alc, fmt, args);
+    defer alc.free(cmd);
+    log.debug("{s}", .{cmd});
+
+    var process_child = process.Child.init(
+        &[_][]const u8{ shell(), "-c", cmd },
+        alc,
+    );
+
+    process_child.stderr_behavior = .Inherit;
+    process_child.stdin_behavior = .Inherit;
+    process_child.stdout_behavior = .Inherit;
+    return try process_child.spawnAndWait();
+}
+
+fn shell() []const u8 {
+    if (posix.getenv("SHELL")) |s| return s;
+    return "sh";
 }
