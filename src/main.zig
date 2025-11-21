e3b8703a43978727c75c146c2410da2b18f6b673
Yunier Alvarez <yunieralvarez@gmail.com>
2025-11-21 04:32:35 -0500

    sync

--- src/main.zig
+++ src/main.zig
@@ -1,1 +1,13 @@
-pub fn main() void {}
+const std = @import("std");
+const head = std.heap;
+
+const sh = @import("shell.zig");
+
+pub fn main() !void {
+    var gpa = head.GeneralPurposeAllocator(.{}){};
+    const alc = gpa.allocator();
+    const res = try sh.exec(alc, "echo abc", .{});
+    defer alc.free(res.stderr);
+    defer alc.free(res.stdout);
+    _ = try sh.execInteractive(alc, "echo abc", .{});
+}
